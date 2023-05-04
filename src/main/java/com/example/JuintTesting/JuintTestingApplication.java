440fee78f1af1cf7761bd07a5c64d9e16e849331
DESKTOP-CFF8UUC\softsuave <udhayakumar.thirunanasambantham@softsuave.com>
2023-05-04 15:26:54 +0530

    Intial commit

---
+++ src/main/java/com/example/JuintTesting/JuintTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.JuintTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuintTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuintTestingApplication.class, args);
+	}
+
+}
