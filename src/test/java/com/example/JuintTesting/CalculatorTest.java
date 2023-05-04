c889c97048e75a088d9a40d8af3c62994c90a075
DESKTOP-CFF8UUC\softsuave <udhayakumar.thirunanasambantham@softsuave.com>
2023-05-04 20:43:55 +0530

    assertIterableEquals

--- src/test/java/com/example/JuintTesting/CalculatorTest.java
+++ src/test/java/com/example/JuintTesting/CalculatorTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CalculatorTest {
 
     Calculator cal = new Calculator();
@@ -19,5 +22,13 @@ public class CalculatorTest {
     @DisplayName("assertArray")
     public void assertArray(){
         Assertions.assertArrayEquals(new int[]{1,2,3} , new int[]{1,2,3});
+    }
+
+    @Test
+    @DisplayName("assertIterable")
+    public void assertIterable(){
+        Iterable<Integer> t1 = new ArrayList<>(Arrays.asList(1,2,3,4));
+        Iterable<Integer> t2 = new ArrayList<>(Arrays.asList(1,2,3,4));
+        Assertions.assertIterableEquals(t1, t2);
     }
 }
