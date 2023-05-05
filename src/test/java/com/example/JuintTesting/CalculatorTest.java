32ac49694d24825516afdc9e2756477645f5176c
DESKTOP-CFF8UUC\softsuave <udhayakumar.thirunanasambantham@softsuave.com>
2023-05-05 12:39:25 +0530

    @Nested

--- src/test/java/com/example/JuintTesting/CalculatorTest.java
+++ src/test/java/com/example/JuintTesting/CalculatorTest.java
@@ -4,6 +4,7 @@ import com.example.JuintTesting.calculator.Calculator;
 import com.example.JuintTesting.calculator.Student;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -13,33 +14,36 @@ public class CalculatorTest {
 
     Calculator cal = new Calculator();
 
-    @Test
-    @DisplayName("Add")
-    public void add(){
-        Assertions.assertNotEquals(78 , cal.add(2,2));
-    }
+    @Nested
+    class Addition{
+        @Test
+        @DisplayName("Add")
+        public void add(){
+            Assertions.assertNotEquals(78 , cal.add(2,2));
+        }
 
-    @Test
-    @DisplayName("assertArray")
-    public void assertArray(){
-        Assertions.assertArrayEquals(new int[]{1,2,3} , new int[]{1,2,3});
-    }
+        @Test
+        @DisplayName("assertArray")
+        public void assertArray(){
+            Assertions.assertArrayEquals(new int[]{1,2,3} , new int[]{1,2,3});
+        }
 
-    @Test
-    @DisplayName("assertIterable")
-    public void assertIterable(){
-        Iterable<Integer> t1 = new ArrayList<>(Arrays.asList(1,2,3,4));
-        Iterable<Integer> t2 = new ArrayList<>(Arrays.asList(1,2,3,4));
-        Assertions.assertIterableEquals(t1, t2);
-    }
+        @Test
+        @DisplayName("assertIterable")
+        public void assertIterable(){
+            Iterable<Integer> t1 = new ArrayList<>(Arrays.asList(1,2,3,4));
+            Iterable<Integer> t2 = new ArrayList<>(Arrays.asList(1,2,3,4));
+            Assertions.assertIterableEquals(t1, t2);
+        }
 
-    @Test
-    @DisplayName("assertNotNull")
-    public void assertNotNull(){
-        String s1 = null;
-        String s2 = "abc";
-        Assertions.assertNull(s1);
-        Assertions.assertNotNull(s2);
+        @Test
+        @DisplayName("assertNotNull")
+        public void assertNotNull(){
+            String s1 = null;
+            String s2 = "abc";
+            Assertions.assertNull(s1);
+            Assertions.assertNotNull(s2);
+        }
     }
 
     @Test
