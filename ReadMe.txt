6d4e13138b38de6f27192c56398730de7c62cf72
DESKTOP-CFF8UUC\softsuave <udhayakumar.thirunanasambantham@softsuave.com>
2023-05-17 10:43:59 +0530

    map and flatMap

--- ReadMe.txt
+++ ReadMe.txt
@@ -3,3 +3,7 @@ Flux.zip(a,b,c)                                                                 
 
 a.zipWith(b)                                                                    -> It is used to create tuple for 2 objects.It will return as a tuple for the 2 objects.
     .map(t -> new String(t.getT1() , t.getT2());                                -> We can access by getT1() , getT2()
+
+studentFlux.map()                                                               -> It takes any Function interface. It will return as Flux.  It  will process synchronously.
+
+studentFlux.flatMap()                                                           -> It takes any Function interface as Publisher. It will return as Flux. It will process Asynchronously.
--- src/test/java/com/example/JuintTesting/ReativeMethods/ReactiveMethods.java
+++ src/test/java/com/example/JuintTesting/ReativeMethods/ReactiveMethods.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.util.function.Tuple2;
 import reactor.util.function.Tuple3;
 
@@ -18,6 +19,23 @@ public class ReactiveMethods {
 
         Flux<Student> studentFlux = name.zipWith(age).map(tuple -> new Student(tuple.getT1(), tuple.getT2()));
         studentFlux.subscribe(System.out::println);
+    }
+
+    @Test
+    public void testMap(){
+        Flux<Student> studentFlux = Flux.just(
+                new Student("udhaya" , 20 , "Tamil"),
+                new Student("Kumar" , 21 , "English"),
+                new Student("Deepak" , 21 , "Maths"),
+                new Student("udhaya1" , 22 , "social"),
+                new Student("Kumar1" , 24 , "Biology"),
+                new Student("Deepak1" , 22 , "science")
+        );
+        Flux<String> map = studentFlux.map(Student::getName);
+        map.subscribe(System.out::println);
+        Flux<String> stringFlux = studentFlux.flatMap(stud -> Flux.just(stud.getName()));
+        stringFlux.subscribe(System.out::println);
+
 
     }
 
