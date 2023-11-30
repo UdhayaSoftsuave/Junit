1510140fe2854ff98f9a2c6f6142d75a417ad4de
DESKTOP-CFF8UUC\softsuave <udhayakumar.thirunanasambantham@softsuave.com>
2023-11-30 18:36:17 +0530

    Custom return type for JPA method using  @SqlResultSetMapping method.

--- src/main/java/com/example/JuintTesting/calculator/StudentResponseDto.java
+++ src/main/java/com/example/JuintTesting/calculator/StudentResponseDto.java
@@ -1,6 +1,27 @@
 package com.example.JuintTesting.calculator;
 
-public interface StudentResponseDto {
-    String getName();
-    String getSchoolName();
+public class StudentResponseDto {
+    private String name;
+    private String schoolName;
+	public StudentResponseDto() {
+		super();
+	}
+	public StudentResponseDto(String name, String schoolName) {
+		super();
+		this.name = name;
+		this.schoolName = schoolName;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSchoolName() {
+		return schoolName;
+	}
+	public void setSchoolName(String schoolName) {
+		this.schoolName = schoolName;
+	}
+	
 }
